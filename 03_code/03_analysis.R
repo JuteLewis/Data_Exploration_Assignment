28c4001119eb5c2e3a080bc46be87d94372bff3e
Glen Lewis <glenlewis05@gmail.com>
2021-02-20 15:57:29 -0800

    Contains the figure build, final RMarkDown Draft.

--- 03_code/03_analysis.R
+++ 03_code/03_analysis.R
@@ -3,85 +3,50 @@ st(wip_institute_data)
 
 ggplot(wip_institute_data, aes(x = sd_earnings)) + 
   geom_boxplot() + 
-  xlab('Reported Earnings Standard Deviations') +
-  geom_vline(xintercept = 1)
-
-ggplot(wip_institute_data, aes(x = sd_earnings)) + 
-  geom_density(fill = 'blue', alpha = .5) + 
-  xlab('Reported Earnings Standard Deviations') +
-  geom_vline(xintercept = 1)
-
-
-base_pre_2015_earning_index %>% filter(sd_earnings > .786) %>% 
-ggplot(aes(x = sd_earnings, y = monthly_std_inst_index)) + 
-  geom_point() +
-  xlab('Standardized Earnings') +
-  ylab('Monthly Standardized Index Score') +
-  ggtitle('High Earning Institiutes Monthly Std Index, Prior to 2015')
-
-base_pre_2015_earning_index %>% filter(sd_earnings < -.159) %>% 
-  ggplot(aes(x = sd_earnings, y = monthly_std_inst_index)) + 
-  geom_point() +
-  xlab('Standardized Earnings') +
-  ylab('Monthly Standardized Index Score') +
-  ggtitle('Low Earning Institiutes Monthly Std Index, Prior to 2015') 
-
-pre_2015_high <- base_pre_2015_earning_index %>% filter(sd_earnings > .786)
-pre_2015_high_lm <- lm(monthly_std_inst_index ~ sd_earnings + month, data= pre_2015_high)
-pre_2015_low <- base_pre_2015_earning_index %>% filter(sd_earnings < -0.159)
-pre_2015_low_lm <- lm(monthly_std_inst_index ~ sd_earnings + month, data= pre_2015_low)
-export_summs(pre_2015_high_lm, pre_2015_low_lm)
-
-effect_plot(pre_2015_low_lm, sd_earnings, plot.points = FALSE)
-
-#######
-ggplot(wip_institute_data, aes(x = sd_earnings)) + 
-  geom_boxplot() + 
-  xlab('Reported Earnings Standard Deviations') 
-
-ggplot(wip_institute_data, aes(x = sd_earnings)) + 
-  geom_density(fill = 'blue', alpha = .5) + 
   xlab('Reported Earnings Standard Deviations')
 
- 
-ggplot(pre_2015_high, aes(x = sd_earnings, y = std_inst_index)) + 
+
+ggplot(wip_institute_data, aes(x = sd_earnings)) + 
+  geom_density(fill = 'blue', alpha = .5) + 
+  xlab('Reported Earnings Standard Deviations') +
+  geom_vline(xintercept = 1)
+
+ggplot(pre_2015_hi, aes(x = sd_earnings, y = monthly_av_stdzed_index_score)) + 
   geom_point() +
   xlab('Standardized Earnings') +
   ylab('Monthly Standardized Index Score') +
   ggtitle('High Earning Institiutes Monthly Std Index, Prior to 2015')
 
-base_pre_2015_earning_index %>% filter(sd_earnings < -.159) %>% 
-  ggplot(aes(x = sd_earnings, y = monthly_std_inst_index)) + 
+ggplot(pre_2015_low, aes(x = sd_earnings, y = monthly_av_stdzed_index_score)) + 
   geom_point() +
   xlab('Standardized Earnings') +
   ylab('Monthly Standardized Index Score') +
   ggtitle('Low Earning Institiutes Monthly Std Index, Prior to 2015') 
 
-pre_2015_high <- base_pre_2015_earning_index %>% filter(sd_earnings > .786)
-pre_2015_high_lm <- lm(std_inst_index ~ sd_earnings + month, data= pre_2015_high)
-export_summs(pre_2015_high_lm)
+st_pre_2015_hi_lm <- lm(monthly_av_stdzed_index_score ~ sd_earnings + month, data = pre_2015_hi)
+poly_pre_2015_hi_lm <- lm(monthly_av_stdzed_index_score ~ sd_earnings + I(sd_earnings^2) + month, data = pre_2015_hi)
+pre_2015_low_lm <- lm(monthly_av_stdzed_index_score ~ sd_earnings + month, data = pre_2015_low)
+export_summs(st_pre_2015_hi_lm, poly_pre_2015_hi_lm, pre_2015_low_lm, model.names = c('pre_2015_hi_lm', 'poly_pre_2015_hi_lm', 'pre_2015_low_lm'))
+effect_plot(st_pre_2015_hi_lm, sd_earnings, plot.points = TRUE, x.label = 'Standardized Earnings', y.label =  'Monthly Standardized Index Scores', point.color = 'blue2', main.title = "Straight pre 2015 Model")
+effect_plot(poly_pre_2015_hi_lm, sd_earnings, plot.points = TRUE, x.label = 'Standardized Earnings', y.label =  'Monthly Standardized Index Scores', point.color = 'blue2', main.title = "Polynomial pre 2015 Model")
 
-pre_2015_low <- base_pre_2015_earning_index %>% filter(sd_earnings < -0.159)
-pre_2015_low_lm <- lm(monthly_std_inst_index ~ sd_earnings + month, data= pre_2015_low)
-export_summs(pre_2015_high_lm, pre_2015_low_lm)
-
-effect_plot(pre_2015_low_lm, sd_earnings, plot.points = FALSE)
-
-####
-
-ggplot(post_2015_high, aes(x = sd_earnings, y = monthly_std_inst_index)) + 
+ggplot(post_2015_hi, aes(x = sd_earnings, y = monthly_av_stdzed_index_score)) + 
   geom_point() +
   xlab('Standardized Earnings') +
   ylab('Monthly Standardized Index Score') +
-  ggtitle('High Earning Institiutes Monthly Std Index, After 2015')
-
-base_post_2015_earnings_index %>% filter(sd_earnings < -.159) %>% 
-  ggplot(aes(x = sd_earnings, y = monthly_std_inst_index)) + 
+  ggtitle('High Earning Institiutes Monthly Std Index, Post 2015')
+ggplot(post_2015_low, aes(x = sd_earnings, y =monthly_av_stdzed_index_score)) + 
   geom_point() +
   xlab('Standardized Earnings') +
   ylab('Monthly Standardized Index Score') +
-  ggtitle('Low Earning Institiutes Monthly Std Index, After 2015')
+  ggtitle('Low Earning Institiutes Monthly Std Index, Post 2015') 
 
-post_2015_high <- base_pre_2015_earning_index %>% filter(sd_earnings > .786)
-post_2015_high_lm <- lm(std_inst_index ~ sd_earnings + month, data= pre_2015_high)
-export_summs(pre_2015_high_lm, post_2015_high_lm)
+poly_post_2015_hi_lm <- lm(monthly_av_stdzed_index_score ~ sd_earnings + I(sd_earnings^2) +  month, data = post_2015_hi)
+post_2015_low_lm <- lm(monthly_av_stdzed_index_score ~ sd_earnings + month, data = post_2015_low)
+export_summs(poly_post_2015_hi_lm, post_2015_low_lm, model.names = c('poly_post_2015_hi_lm', 'post_2015_low_lm'))
+effect_plot(poly_post_2015_hi_lm, sd_earnings, plot.points = TRUE, x.label = 'Standardized Earnings', y.label =  'Monthly Standardized Index Scores', point.color = 'blue2', main.title = "Polynomial post 2015 Model")
+
+poly_post_2015_hi_lm <- lm(monthly_av_stdzed_index_score ~ sd_earnings + I(sd_earnings^2) +  month, data = post_2015_hi)
+post_2015_low_lm <- lm(monthly_av_stdzed_index_score ~ sd_earnings + month, data = post_2015_low)
+export_summs(poly_post_2015_hi_lm, post_2015_low_lm, model.names = c('poly_post_2015_hi_lm', 'post_2015_low_lm'))
+effect_plot(poly_post_2015_hi_lm, sd_earnings, plot.points = TRUE, x.label = 'Standardized Earnings', y.label =  'Monthly Standardized Index Scores', point.color = 'blue2', main.title = "Polynomial post 2015 Model")
